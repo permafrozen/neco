241cad519e5e4e0477d81661be9f4dfd288e5fe4
Permafrozen <m8tt8o@gmail.com>
2025-08-24 21:59:56 +0200

    nixvim flake init
    direnv provided nix-shell and nixvim package now exposed (sigma)

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # neco
 evil neovim configuration via standalone nixvim >:)
+
+# dev
+To get started clone the repo and optionally make sure `direnv` is installed.
+```console
+git clone git@github.com:permafrozen/neco.git
+cd ./neco
+direnv allow
+```
+
+`direnv` is used to provide a nix-shell environment with the built config from this flake as a `nvim` binary. This can be used to test changes on the fly.
+
+# misc
+
+repo used the flake template provided by `nixvim`
+
+```console
+nix flake init -t github:nix-community/nixvim
+```
+
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A nixvim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    { nixvim, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { system, pkgs, ... }:
+        let
+          nixvimLib = nixvim.lib.${system};
+          nixvim' = nixvim.legacyPackages.${system};
+          nixvimModule = {
+            inherit system; # or alternatively, set `pkgs`
+            module = import ./config; # import the module directly
+            # You can use `extraSpecialArgs` to pass additional arguments to your module files
+            extraSpecialArgs = {
+              # inherit (inputs) foo;
+            };
+          };
+          nvim = nixvim'.makeNixvimWithModule nixvimModule;
+        in
+        {
+          checks.default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule; # check config w/ `nix flake check .`
+          packages.default = nvim;
+          devShells.default = pkgs.mkShell { packages = [ nvim ]; };
+        };
+    };
+}
